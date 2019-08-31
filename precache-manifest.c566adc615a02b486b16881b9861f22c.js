ade4057270140991952ee908b434d97119ac7947
xiaobin900114 <17800697+xiaobin900114@users.noreply.github.com>
2019-08-31 10:18:59 -0400

    Updates

---
+++ precache-manifest.c566adc615a02b486b16881b9861f22c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff94f8b3e97d89d84c1541f1399dbe4",
+    "url": "/spotify_playlist/index.html"
+  },
+  {
+    "revision": "1da39b43ea36929d7421",
+    "url": "/spotify_playlist/static/css/main.8e688f82.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/spotify_playlist/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "1da39b43ea36929d7421",
+    "url": "/spotify_playlist/static/js/main.6785afcd.chunk.js"
+  },
+  {
+    "revision": "4cbcef4a6a7f846763f9",
+    "url": "/spotify_playlist/static/js/runtime~main.8afe465e.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/spotify_playlist/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
