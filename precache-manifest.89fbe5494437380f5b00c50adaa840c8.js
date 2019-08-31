5675726028562c772220faa6e02c553dceaa3e5e
xiaobin900114 <17800697+xiaobin900114@users.noreply.github.com>
2019-08-31 10:17:07 -0400

    Updates

---
+++ precache-manifest.89fbe5494437380f5b00c50adaa840c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4933d49fd454d5963038eda3081e747c",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "023e6141c92efbc4f2ca",
+    "url": "/jammming/static/css/main.79756365.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/jammming/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "023e6141c92efbc4f2ca",
+    "url": "/jammming/static/js/main.467aa39c.chunk.js"
+  },
+  {
+    "revision": "f5f9d50d5961f1446a57",
+    "url": "/jammming/static/js/runtime~main.85c7c486.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
